eef52c06eb26ebb078a92e9fbb0b528b819bbb84
mic-joebstl <147158051+mic-joebstl@users.noreply.github.com>
2023-10-06 15:19:52 +0200

    Smothed mockup move command

--- LaserEngraver.Core/Devices/MockDevice.cs
+++ LaserEngraver.Core/Devices/MockDevice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -73,17 +74,34 @@ namespace LaserPathEngraver.Core.Devices
 				Status = DeviceStatus.Executing;
 			}
 
-			var oldPosition = Position;
-			if (oldPosition != null)
+			var sw = new Stopwatch();
+			sw.Start();
+			long i = 0;
+			while (Position != null && (Position.Value.X != position.X || Position.Value.Y != position.Y))
 			{
 				var vector = new Point
 				{
-					X = position.X - oldPosition.Value.X,
-					Y = position.Y - oldPosition.Value.Y
+					X = position.X - Position.Value.X,
+					Y = position.Y - Position.Value.Y
 				};
-				await Task.Delay(Math.Abs(vector.X) * TimeSpan.FromMilliseconds(1) + Math.Abs(vector.Y) * TimeSpan.FromMilliseconds(1), cancellationToken);
-				Position = position;
+				var unitVector = new Point
+				{
+					X = vector.X != 0 ? vector.X / Math.Abs(vector.X) : 0,
+					Y = vector.Y != 0 ? vector.Y / Math.Abs(vector.Y) : 0
+				};
+
+				if (++i > sw.ElapsedMilliseconds / 2)
+				{
+					await Task.Delay(TimeSpan.FromMilliseconds(i - sw.ElapsedMilliseconds / 2), cancellationToken);
+
+				}
+				Position = new Point
+				{
+					X = Position.Value.X + unitVector.X,
+					Y = Position.Value.Y + unitVector.Y,
+				};
 			}
+			sw.Stop();
 
 			Status = DeviceStatus.Ready;
 		}
