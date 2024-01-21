bde64bd08f16394e33a59d98813040150a3e1940
mic-joebstl <147158051+mic-joebstl@users.noreply.github.com>
2024-01-21 17:11:48 +0100

    Changed point animation storyboard: the animation duration is now estimated based on the travel vector length. Very short travel distances are not animated at all. If the queue gets too long, animations are sped up.

--- src/UI.Win/Visuals/BurnTarget.cs
+++ src/UI.Win/Visuals/BurnTarget.cs
@@ -1,6 +1,7 @@
 ﻿using LaserEngraver.UI.Win.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,7 @@ namespace LaserEngraver.UI.Win.Visuals
 		public BurnTarget()
 		{
 			_circle = new System.Windows.Shapes.Ellipse();
-			_positionStoryBoard = new PointAnimationStoryBoard { Limit = 2 };
+			_positionStoryBoard = new PointAnimationStoryBoard { Limit = 10 };
 		}
 
 		public Point Position
@@ -33,9 +34,7 @@ namespace LaserEngraver.UI.Win.Visuals
 					_positionStoryBoard.Enqueue(new PointAnimation
 					{
 						From = _position,
-						To = value,
-						Duration = TimeSpan.FromSeconds(.25),
-						AnimationTimingFunction = AnimationTimingFunction.EaseInOut
+						To = value
 					});
 					_position = value;
 				}
@@ -73,19 +72,22 @@ namespace LaserEngraver.UI.Win.Visuals
 
 		#endregion
 
-		private enum AnimationTimingFunction 
+		private enum AnimationTimingFunction
 		{
 			Linear = 0,
-			EaseInOut = 1
+			EaseIn = 1,
+			EaseOut = 2,
+			EaseInOut = 3,
 		}
 		private class PointAnimation
 		{
-			private DateTime _startTime;
 			private bool _hasStarted;
 			private bool _hasFinished;
+			private Stopwatch _stopWatch = new Stopwatch();
 
 			public Point From { get; set; }
 			public Point To { get; set; }
+			public double SpeedFactor { get; set; }
 			public TimeSpan Duration { get; set; }
 			public bool HasFinished
 			{
@@ -100,12 +102,12 @@ namespace LaserEngraver.UI.Win.Visuals
 						return _hasFinished;
 					}
 
-					var now = DateTime.UtcNow;
-					var duration = Duration.TotalMilliseconds;
-					var t = (now - _startTime).TotalMilliseconds;
-					if (t > duration)
+					var duration = Duration.TotalMilliseconds * SpeedFactor;
+					var t = _stopWatch.ElapsedMilliseconds;
+					if (t >= duration)
 					{
 						_hasFinished = true;
+						_stopWatch.Stop();
 					}
 
 					return _hasFinished;
@@ -125,9 +127,13 @@ namespace LaserEngraver.UI.Win.Visuals
 						return To;
 					}
 
-					var duration = Duration.TotalMilliseconds;
-					var t = (DateTime.UtcNow - _startTime).TotalMilliseconds;
-					t = AnimationTimingFunction == AnimationTimingFunction.EaseInOut ? t * EaseInOut(t / duration) : t;
+					var duration = Duration.TotalMilliseconds * SpeedFactor;
+					var t = (double)_stopWatch.ElapsedMilliseconds;
+					t =
+						AnimationTimingFunction == AnimationTimingFunction.EaseInOut ? t * EaseInOut(t / duration) :
+						AnimationTimingFunction == AnimationTimingFunction.EaseIn ? t * EaseIn(t / duration) :
+						AnimationTimingFunction == AnimationTimingFunction.EaseOut ? t * EaseOut(t / duration) :
+						t;
 
 					var vector = To - From;
 					var vectorT = vector / duration;
@@ -138,18 +144,27 @@ namespace LaserEngraver.UI.Win.Visuals
 
 			public bool TryStart()
 			{
-				if(_hasStarted)
+				if (_hasStarted)
 				{
 					return false;
 				}
 				_hasStarted = true;
-				_startTime = DateTime.UtcNow;
+				_stopWatch.Start();
 
 				return true;
 			}
 
+			private static double EaseIn(double progress)
+				=> progress < 0 ? 0 : progress > 1 ? 1 :
+				   1 - EaseOut(1 - progress);
+
+			private static double EaseOut(double progress)
+				=> progress < 0 ? 0 : progress > 1 ? 1 :
+				   progress * progress * progress;
+
 			private static double EaseInOut(double progress)
-				=> progress < 0.5 ? 4 * progress * progress * progress : 1 - Math.Pow(-2 * progress + 2, 3) / 2;
+				=> progress < 0 ? 0 : progress > 1 ? 1 :
+				   progress < 0.5 ? 4 * progress * progress * progress : 1 - Math.Pow(-2 * progress + 2, 3) / 2;
 		}
 
 		private class PointAnimationStoryBoard
@@ -157,6 +172,13 @@ namespace LaserEngraver.UI.Win.Visuals
 			private readonly object _sync = new object();
 			private readonly Queue<PointAnimation> _queue = new Queue<PointAnimation>();
 			private PointAnimation _currentAnimation = new PointAnimation();
+			private PointAnimation _lastAnimation;
+
+			public PointAnimationStoryBoard()
+			{
+				_lastAnimation = _currentAnimation;
+				_lastAnimation.TryStart();
+			}
 
 			public int Limit { get; set; }
 			public PointAnimation CurrentAnimation
@@ -165,7 +187,7 @@ namespace LaserEngraver.UI.Win.Visuals
 				{
 					lock (_sync)
 					{
-						EnsureLimit();
+						EnsureCurrent();
 						if (_queue.TryPeek(out var current))
 						{
 							current.TryStart();
@@ -179,15 +201,40 @@ namespace LaserEngraver.UI.Win.Visuals
 			{
 				lock (_sync)
 				{
-					_queue.Enqueue(animation);
-					EnsureLimit();
+					var length = (animation.To - animation.From).Length;
+					animation.Duration = (TimeSpan.FromSeconds(0.5) / 1600) * length;
+
+					if (_lastAnimation.HasFinished || animation.Duration > TimeSpan.FromMilliseconds(10) || _lastAnimation.Duration > TimeSpan.FromMilliseconds(10))
+					{
+						_queue.Enqueue(animation);
+						_lastAnimation = animation;
+					}
+					else
+					{
+						_lastAnimation.To = animation.To;
+					}
+
+					EnsureCurrent();
 				}
 			}
 
-			private void EnsureLimit()
+			private void EnsureCurrent()
 			{
+				var count = _queue.Count;
 				var limit = Limit;
-				while (_queue.TryPeek(out var current))
+				var i = 0;
+				foreach (var animation in _queue)
+				{
+					i++;
+					var x = count > limit ? limit : count;
+					animation.SpeedFactor = Math.Clamp(Math.Round(Math.Cos(x * (Math.PI / (2 * limit))), 2), 0, 1);
+					animation.AnimationTimingFunction =
+						count == 1 ? AnimationTimingFunction.EaseInOut :
+						i == count ? AnimationTimingFunction.EaseOut :
+						AnimationTimingFunction.Linear;
+				}
+
+				while (_queue.TryPeek(out var current) && _queue.Count > 1)
 				{
 					if (current.HasFinished)
 					{
@@ -198,7 +245,6 @@ namespace LaserEngraver.UI.Win.Visuals
 						break;
 					}
 				}
-				while (_queue.Count > limit && _queue.TryDequeue(out _)) ;
 			}
 		}
 	}
